90e61f18b032f3a57569bfd357a31988c22baee4
Rhomulo Mourao <rhomulocmourao@gmail.com>
2022-10-08 12:59:11 -0300

    Atualização

---
+++ Lista5/Ex1Lista5.java
@@ -0,0 +1,26 @@
+/*
+ * Ex1Lista5.java
+
+ */
+//Rhomulo Mourao Caitano dos Santos
+
+
+import java.util.Scanner;
+public class Ex1Lista5 {
+	
+	public static void main (String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		
+		System.out.print("Digite um numero: ");
+		int numero = teclado.nextInt();
+		
+		if(numero > 10){
+			System.out.println("E MAIOR QUE 10");
+			}else{
+				System.out.println("NAO E MAIOR QUE 10");
+				}
+		
+		
+	}
+}
+
